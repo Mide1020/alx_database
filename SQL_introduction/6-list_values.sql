55061644cc572a3c7d3284b3b08e7316614e436e
mide1020 <akinrefonayomide1@gmail.com>
2023-08-18 11:42:25 +0100

    task 6

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,5 +1,11 @@
 -- List all rows from the table first_table
-CREATE DATABASE hbtn_0c_0;
 USE hbtn_0c_0;
+
+CREATE TABLE first_table (
+    id INT,
+    name VARCHAR(256),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
 
 SELECT * FROM first_table;
