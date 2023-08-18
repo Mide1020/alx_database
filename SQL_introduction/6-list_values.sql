d96bb611cfe62de8656e6a0c480958f9c9a9ac13
mide1020 <akinrefonayomide1@gmail.com>
2023-08-18 11:48:00 +0100

    task 6

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,11 +1,5 @@
 -- List all rows from the table first_table
 USE hbtn_0c_0;
 
-CREATE TABLE first_table (
-    id INT,
-    name VARCHAR(256),
-    created_at DATE,
-    PRIMARY KEY (id)
-);
-
-SELECT * FROM first_table;
+SELECT CONCAT(id, name) AS output
+FROM first_table
