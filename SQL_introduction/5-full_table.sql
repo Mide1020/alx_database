5b278986d8379175626d7668eae3f4fd58a9c293
mide1020 <akinrefonayomide1@gmail.com>
2023-08-18 09:32:05 +0100

    task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- List the columns and their data types of the table first_table in the specified database
-SELECT COLUMN_NAME, DATA_TYPE
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = '$1' AND TABLE_NAME = 'first_table';
+SHOW COLUMN FROM
+hbtn_0c_0.first_table;
