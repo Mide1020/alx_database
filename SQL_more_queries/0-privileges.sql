74ca7cb37ea671b4a36d85f2fb063ee8a1776e60
mide1020 <akinrefonayomide1@gmail.com>
2023-08-25 16:01:25 +0100

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,14 @@
--- This script lists the privileges of MySQL users user_0d_1 and user_0d_2 on localhost.
+#!/bin/bash
 
--- Prompt the user to enter the MySQL root password
-read -s -p "Enter MySQL root password: " mysql_root_password
-echo
+-- MySQL user details
+mysql_user_1="user_0d_1"
+mysql_user_2="user_0d_2"
+
+-- MySQL root password
+mysql_root_password="ayomide"
 
 -- Command to list privileges for user_0d_1
-mysql -h localhost -u root -p$mysql_root_password -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
+mysql -h localhost -u root -p$mysql_root_password -e "SHOW GRANTS FOR '$mysql_user_1'@'localhost';"
 
 -- Command to list privileges for user_0d_2
-mysql -h localhost -u root -p$mysql_root_password -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+mysql -h localhost -u root -p$mysql_root_password -e "SHOW GRANTS FOR '$mysql_user_2'@'localhost';"
