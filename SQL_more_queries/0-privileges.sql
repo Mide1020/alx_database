ad7ac5b8bb74265a2e31ebefd24d0139dfcc3296
mide1020 <akinrefonayomide1@gmail.com>
2023-08-25 16:05:20 +0100

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,14 +1,14 @@
 #!/bin/bash
 
--- MySQL user details
+# MySQL user details
 mysql_user_1="user_0d_1"
 mysql_user_2="user_0d_2"
 
--- MySQL root password
+# MySQL root password
 mysql_root_password="ayomide"
 
--- Command to list privileges for user_0d_1
+# Command to list privileges for user_0d_1
 mysql -h localhost -u root -p$mysql_root_password -e "SHOW GRANTS FOR '$mysql_user_1'@'localhost';"
 
--- Command to list privileges for user_0d_2
+# Command to list privileges for user_0d_2
 mysql -h localhost -u root -p$mysql_root_password -e "SHOW GRANTS FOR '$mysql_user_2'@'localhost';"
