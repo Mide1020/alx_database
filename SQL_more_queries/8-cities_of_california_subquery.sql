1bab6047618b169247dd605f5d67ebda54989dd4
mide1020 <akinrefonayomide1@gmail.com>
2023-08-29 00:04:32 +0100

    task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,5 @@
--- This script assumes you're passing the database name as an argument
--- Replace 'your_database_name' with the actual database name in the command line
-
 -- Use the specified database
-USE your_database_name;
+USE hbtn_0d_usa;
 
 -- List cities of California
 SELECT cities.id, cities.name
