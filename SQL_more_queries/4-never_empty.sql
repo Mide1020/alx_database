a8c79f1e88d044b881b3a788e04459ebaa4eb07d
mide1020 <akinrefonayomide1@gmail.com>
2023-08-28 23:35:45 +0100

    task 4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- Replace 'your_database_name' with the actual database name where you want to create the table
-USE your_database_name;
+USE your_database_name; hbtn_test_db_4.id_not_null
 
 -- Create table id_not_null if not exists
 CREATE TABLE IF NOT EXISTS id_not_null (
