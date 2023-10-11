14f4d8880d2933429568d33afbe94c77f776b2db
mide1020 <akinrefonayomide1@gmail.com>
2023-10-11 03:01:14 +0100

    correct

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,8 +1,5 @@
--- Replace 'your_database_name' with the actual database name where you want to create the table
-USE your_database_name;
-
--- Create table force_name if not exists
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY,
+-- Creating a table in my Mysql server
+CREATE TABLE IF NOT EXISTS force_name(
+    id INT,
     name VARCHAR(256) NOT NULL
 );
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,18 +1,5 @@
--- Create database hbtn_0d_usa if not exists
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Use the hbtn_0d_usa database
-USE hbtn_0d_usa;
-
--- Create table cities if not exists
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL,
+-- Creating a table in my Mysql server
+CREATE TABLE IF NOT EXISTS force_name(
+    id INT,
     name VARCHAR(256) NOT NULL
 );
-
--- List cities of California
-SELECT cities.id, cities.name
-FROM cities, states
-WHERE cities.state_id = states.id AND states.name = 'California'
-ORDER BY cities.id ASC;
